ba78a83013d16447f98458f5ca1e0d53e7347394
John <johnimler@envisioncode.com>
2025-07-06 10:51:16 -0400

    Solution: #14 Longest Common Prefix problem solved.
    - Includes common test cases, use vendor/bin/phpunit tests/LongestCommonPrefixTest.php to test.

---
+++ tests/LongestCommonPrefixTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use John\LeetcodePhp\Solutions\longestCommonPrefix\longestCommonPrefix;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * To run tests execute PHPUnit via command:
+ *      vendor/bin/phpunit tests/LongestCommonPrefixTest.php
+ */
+class longestCommonPrefixTest extends TestCase
+{
+    #[DataProvider('inputProvider')]
+    public function testLongestCommonPrefix(string $firstWord, string $secondWord, string $thirdWord, string $expected): void {
+        $longestCommonPrefix = new LongestCommonPrefix();
+
+        $inputArray = [
+            $firstWord,
+            $secondWord,
+            $thirdWord
+        ];
+
+        // Test case 1
+        $this->assertEquals($expected, $longestCommonPrefix->LongestCommonPrefix($inputArray));
+
+        // Test case 2
+        $this->assertEquals($expected, $longestCommonPrefix->LongestCommonPrefix($inputArray));
+
+        // Test case 3
+        $this->assertEquals($expected, $longestCommonPrefix->LongestCommonPrefix($inputArray));
+    }
+
+    public static function inputProvider(): Generator {
+        yield (["flower","flow","flight", "fl"]);
+        yield ["dog","racecar","car", ""];
+    }
+}
